65febc6d085bb681c01d673e230bf8bb962e3cee
GuruneLee <dlckdgk4858@gmail.com>
2020-12-09 20:53:00 +0900

    전화번호-목록

---
+++ 프로그래머스/코딩테스트연습/해시/전화번호-목록/Readme.md
@@ -0,0 +1,6 @@
+# 전화번호 목록
+https://programmers.co.kr/learn/courses/30/lessons/42577
+
+## Solution (C++)
+1. 각 스트링의 각 접두어가 배열안에 존재하는지 확인하기 -> C++은 각 스트링의 각 접두어를 뽑아내는것도 일일듯 ㅋ
+2. Tree -> 이거 안될거 같음...
---
+++ 프로그래머스/코딩테스트연습/해시/전화번호-목록/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+//없으면 true
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    //sort(phone_book.begin(), phone_book.end(), sortFunc);
+    
+    return answer;
+}
+
+/*
+bool sortFunc(string s1, string s2) {
+    return s1.size() < s2.size();
+}
+*/
