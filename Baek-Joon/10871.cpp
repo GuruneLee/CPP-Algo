7edf84f3782ef0be5aeb31a073d3ff79dcf7192a
gurunelee <dlckdgk4858@gmail.com>
2024-01-29 21:59:15 +0900

    알고리즘 강의자료

---
+++ Baek-Joon/10871.cpp
@@ -0,0 +1,16 @@
+// X 보다 작은 수
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        if (tmp < x) {
+            cout << tmp << " ";
+        }
+    }
+}
